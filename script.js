5fdb8ee5526e77fdb65903a0a16bd524dd839aa9
Vinicius <viniipvp@gmail.com>
2025-03-24 23:51:12 -0300

    Acessing current tax value as a float

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-fetch("https://api.bcb.gov.br/dados/serie/bcdata.sgs.4189/dados?formato=json",{
-    method: 'GET',
-    headers: {'Accept': 'application/json'},
-})
-.then(response => response.json()).then(response=> console.log(JSON.stringify(response[response.length-1])))
+let x;
+
+async function getCurrSelicTaxAndDate() {
+    const resposta = await fetch("https://api.bcb.gov.br/dados/serie/bcdata.sgs.4189/dados?formato=json", {
+        method: 'GET',
+        headers: { 'Accept': 'application/json' },
+    })
+
+    var lastValues = await resposta.json();
+    lastValues = (lastValues[lastValues.length - 1])
+    return lastValues; // Returns a JSON Object
+}
+
+async function getCurrSelicTax() {
+    var currentTax = await getCurrSelicTaxAndDate();
+    currentTax = parseFloat(currentTax.valor);
+    console.log(typeof currentTax);
+    return currentTax;
+}
